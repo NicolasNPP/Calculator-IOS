06c53ea9289ea287928da0b4c11ff2daf6bd2fe1
Nicolas Pepe <Nicolasnahuel94@gmail.com>
2023-01-26 13:09:30 -0300

    Add references

--- CalculatorIOS/Controller/ViewController.swift
+++ CalculatorIOS/Controller/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Oulets
+    
+    //Result
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    //Numbers
+    @IBOutlet weak var number1: UIButton!
+    @IBOutlet weak var numer2: UIButton!
+    @IBOutlet weak var number3: UIButton!
+    @IBOutlet weak var number4: UIButton!
+    @IBOutlet weak var number5: UIButton!
+    @IBOutlet weak var number6: UIButton!
+    @IBOutlet weak var number7: UIButton!
+    @IBOutlet weak var number8: UIButton!
+    @IBOutlet weak var number9: UIButton!
+    @IBOutlet weak var numberDecimall: UIButton!
+    
+    //Operators
+    @IBOutlet weak var operatorAc: UIButton!
+    @IBOutlet weak var operatorPlusMinus: UIButton!
+    @IBOutlet weak var operatePercent: UIButton!
+    @IBOutlet weak var operateResult: UIButton!
+    @IBOutlet weak var operateAddition: UIButton!
+    @IBOutlet weak var operateSubstraction: UIButton!
+    @IBOutlet weak var operateMultiplication: UIButton!
+    @IBOutlet weak var operateDivison: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
